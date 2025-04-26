580e83d13d497e13d3ebc8db6cd585e655a58990
Eben <bonuzx1@gmail.com>
2025-04-26 16:54:00 +0100

    feat: Remove unused HowItWorks and Testimonials sections from the project

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import PremiumServices from "./sections/PremiumServices";
 import FAQ from "./sections/FAQ";
 import Opportunity from "./sections/Opportunity";
 import Footer from "./sections/Footer";
-import Testimonials from "./sections/Testimonials";
+// import Testimonials from "./sections/Testimonials";
+// import HowItWorks from "./sections/HowItWorks";
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
       <main id="main-content">
         <Hero />
         <PremiumServices />
-        <Testimonials />
+        {/* <HowItWorks />
+        <Testimonials /> */}
         <FAQ />
         <Opportunity />
       </main>
